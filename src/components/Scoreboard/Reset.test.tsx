106df729f25538d4e43095351d88a41cb4d7bb15
SorinC6 <kish.sorin@yahoo.com>
2022-05-20 14:37:51 +0300

    added more test cases for reset component

--- src/components/Scoreboard/Reset.test.tsx
+++ src/components/Scoreboard/Reset.test.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import { Reset } from './Reset';
 
 describe('Reset button Check', () => {
+  const ResetWIthDummyHandlerOnReset: FC = () => <Reset onReset={() => null} />;
+
   test('Should Render elements with default state', () => {
     render(<Reset onReset={() => null} />);
     expect(screen.getByText('🙂')).toBeInTheDocument();
@@ -14,5 +16,21 @@ describe('Reset button Check', () => {
     render(<Reset onReset={onReset} />);
     fireEvent.click(screen.getByText('🙂'));
     expect(onReset).toBeCalled();
+  });
+
+  test('Should Change state when onMouseDowon and onMuseUp events are triggerd', () => {
+    render(<ResetWIthDummyHandlerOnReset />);
+    fireEvent.mouseDown(screen.getByText('🙂'));
+    expect(screen.getByText('😯')).toBeInTheDocument();
+    fireEvent.mouseUp(screen.getByText('😯'));
+    expect(screen.getByText('🙂')).toBeInTheDocument();
+  });
+
+  test('Should chnage state when mouse down and mouse leave events are happening', () => {
+    render(<ResetWIthDummyHandlerOnReset />);
+    fireEvent.mouseDown(screen.getByText('🙂'));
+    expect(screen.getByText('😯')).toBeInTheDocument();
+    fireEvent.mouseLeave(screen.getByText('😯'));
+    expect(screen.getByText('🙂')).toBeInTheDocument();
   });
 });
