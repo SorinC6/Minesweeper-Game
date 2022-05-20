4cea7aad65b6491135e5f92bb7de85b3578427dd
SorinC6 <kish.sorin@yahoo.com>
2022-05-20 14:58:04 +0300

    added scoreboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import { Top } from './components/Top';
+import { Scoreboard } from './components/Scoreboard';
 
 const App: FC = () => {
   return (
-    <div>
+    <>
       <Top feature="Flag" firstAction="ctrl" secondAction="click">
         Minesweeper
       </Top>
-    </div>
+      <Scoreboard time="000" levels={['beginner', 'intermediate', 'expert']} mines="010" onReset={() => null} />
+    </>
   );
 };
 
